bdccf102e0adc9ade54f027f402b31b0b56fe072
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2024-04-15 22:41:42 +0530

    Time: 815 ms (16.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,9 @@
+SELECT id, COUNT(*) AS num 
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id FROM RequestAccepted
+) AS friends_count
+GROUP BY id
+ORDER BY num DESC 
+LIMIT 1;
